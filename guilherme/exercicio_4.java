1722aac4fb66c8497a29fcbafc86bd8011085be3
Lionkkjk <guilhermelimarocha06@gmail.com>
2026-02-22 13:30:52 -0300

    Exercicio4

---
+++ guilherme/exercicio_4.java
@@ -0,0 +1,28 @@
+package venolas4;
+import java.util.Scanner;
+public class Venolas4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // "admin" e a senha é "1234". Exiba "Acesso concedido" ou "Falha na segurança".
+
+        //Fiz print dos dados e criei objetos correspondentes(cuidado com a ordem!!!).
+        System.out.print("Digite seu usuario:");
+        String name = sc.nextLine();  
+        System.out.print("Digite a senha:");
+        String senha = ("1234");
+        sc.nextLine();
+        
+        //Atribui que o "name" é admin e somente admin.        
+        if (!name.equals("admin")){
+            System.out.print("Falha na segurança");
+        }
+        //se a senha não for ("1234").
+        else {       
+            System.out.print("Acesso concedido");
+        }
+        
+    }
+    
+}
+//Verifique se o usuário é "admin" e a senha é "1234". Exiba "Acesso concedido" ou "Falha na segurança".
+//Leve em conta a ordem das variaveis!!
